99103ede3dff9e04a45716995f4fff331a65704e
Umar Ahmed <umar@chainpatrol.io>
2026-06-13 01:50:35 -0400

    wip: deploy working contract

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 // TODO: Update these with your deployed package IDs after running `sui client publish`
 export const DEVNET_COUNTER_PACKAGE_ID: string | undefined = undefined;
-export const TESTNET_COUNTER_PACKAGE_ID: string | undefined = undefined;
+export const TESTNET_COUNTER_PACKAGE_ID: string | undefined = "0xc879cba7dcaceeed0c19980f7cb6fd79a4b655df5980436b8664ea42cfc91449";
 export const MAINNET_COUNTER_PACKAGE_ID: string | undefined = undefined;
